25704bbd7f8abf68fdbe4739d7c2b5953bced9ac
noemi <noemi@macbookair.homenet.telecomitalia.it>
2025-05-19 22:14:06 +0200

    Updated Test

--- Group2_SAD/src/test/java/sadprojectwork/PaintControllerTest.java
+++ Group2_SAD/src/test/java/sadprojectwork/PaintControllerTest.java
@@ -12,6 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -69,9 +70,9 @@ public class PaintControllerTest {
     @Test
     void testEllipseSelected(FxRobot robot) {
         Platform.runLater(() -> {
-            MyShape ellipse = new MyEllipse(50, 50, 80, 60);
-            ellipse.getFxShape().setFill(Color.GREEN);
-            ellipse.getFxShape().setStroke(Color.YELLOW);
+            MyShape ellipse = new MyEllipse(50, 60, 35, 20);
+            ellipse.getFxShape().setFill(Color.BLUE);
+            ellipse.getFxShape().setStroke(Color.PURPLE);
             controller.getModel().addShape(ellipse);
             controller.enableSelection(ellipse);
         });
@@ -85,4 +86,64 @@ public class PaintControllerTest {
         assertTrue(selected.getFxShape().getEffect() instanceof DropShadow, "If the ellipse is selected, there must be a selection effect!");
     }
     
+    @Test
+    void testLineSelected(FxRobot robot) {
+        Platform.runLater(() -> {
+            MyShape line = new MyLine(0, 0, 30, 20);
+            line.getFxShape().setStroke(Color.BLACK);
+            controller.getModel().addShape(line);
+            controller.enableSelection(line);
+        });
+
+        robot.interact(() -> {});
+
+        robot.moveTo(controller.getCanvas().getChildren().get(0)).clickOn();
+
+        MyShape selected = controller.getSelectedShape();
+        assertNotNull(selected, "The line must be selected!");
+        assertTrue(selected.getFxShape().getEffect() instanceof DropShadow, "If the line is selected, there must be a selection effect!");
+    }
+    
+    @Test
+    void testDisableSelection(FxRobot robot){
+       Platform.runLater(() -> {
+            MyShape rectangle = new MyRectangle(100, 100, 40, 30);
+            rectangle.getFxShape().setFill(Color.BLUE);
+            rectangle.getFxShape().setStroke(Color.PURPLE);
+            controller.getModel().addShape(rectangle);
+            controller.enableSelection(rectangle);
+        });
+
+        robot.interact(() -> {});
+        robot.moveTo(controller.getCanvas().getChildren().get(0)).clickOn();
+
+        robot.clickOn(300, 300, MouseButton.PRIMARY); 
+        assertNull(controller.getSelectedShape(), "After the click, the shape should no longer be selected!");
+    }
+    
+    @Test
+    public void testNewSelectionWithShapeSelected(FxRobot robot){
+        Platform.runLater(() -> {
+            MyShape rectangle = new MyRectangle(40, 40, 30, 30);
+            rectangle.getFxShape().setFill(Color.GREEN);
+            rectangle.getFxShape().setStroke(Color.YELLOW);
+            MyShape ellipse = new MyEllipse(100, 100, 40, 40);
+            ellipse.getFxShape().setFill(Color.BLUE);
+            ellipse.getFxShape().setStroke(Color.PURPLE);
+            controller.getModel().addShape(rectangle);
+            controller.getModel().addShape(ellipse);
+            controller.enableSelection(rectangle);
+            controller.enableSelection(ellipse);
+        });
+
+        robot.interact(() -> {});
+        robot.moveTo(controller.getCanvas().getChildren().get(0)).clickOn();
+        MyShape rect = controller.getSelectedShape();
+
+        robot.moveTo(controller.getCanvas().getChildren().get(1)).clickOn();
+        MyShape ell = controller.getSelectedShape();
+
+        assertNotEquals(rect, ell, "The second shape must be selected and highlighted!");
+        assertNull(rect.getFxShape().getEffect(), "The first shape should no longer be selected and highlighted!");
+    }
 }
